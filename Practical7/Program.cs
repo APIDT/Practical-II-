5fe50d37671197ba47dc3e1d8dde2aaf030c84b0
Bend <144515341+APIDT@users.noreply.github.com>
2025-06-13 22:21:06 +0300

    Update Program.cs

--- Practical7/Program.cs
+++ Practical7/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+class Practice7_Var2
+{
+    static void Main()
+    {
+        Console.Write("Введіть розмір квадратної матриці (N): ");
+        int n = int.Parse(Console.ReadLine());
+
+        if (n <= 0)
+        {
+            Console.WriteLine("Розмір має бути додатнім!");
+            return;
+        }
+
+        int[,] matrix = new int[n, n];
+        Random rand = new Random();
+
+        Console.WriteLine("\nЗгенерована матриця:");
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                matrix[i, j] = rand.Next(1, 10); 
+                Console.Write($"{matrix[i, j],4}");
+            }
+            Console.WriteLine();
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            int sum = matrix[0, i] + matrix[n - 1, i];
+            matrix[i, n - 1] = sum; 
+        }
+
+        Console.WriteLine("\nМодифікована матриця:");
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write($"{matrix[i, j],4}");
+            }
+            Console.WriteLine();
+        }
+    }
+}
